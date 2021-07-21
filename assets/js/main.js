e8b9b218b213cf1747be4b10069a2caf7bad71e6
Kawakami <fukabekonami@gmail.com>
2021-07-21 16:22:44 +0900

    faqのアコーディオンの動き追加

--- assets/js/main.js
+++ assets/js/main.js
@@ -11,3 +11,20 @@ $(document).ready(function(){
         speed: 5000,
     });
   });
+
+ /******************************************************************************
+ faq accordion 
+******************************************************************************/
+$('.faq-contents-item').click(function(){
+
+    var $answer = $(this).find('.faq-contents-item-answer');
+
+    if($answer.hasClass('open')){
+        $answer.removeClass('open');
+        $answer.slideUp();
+    }
+    else{
+        $answer.addClass('open');
+        $answer.slideDown();
+    }
+});
