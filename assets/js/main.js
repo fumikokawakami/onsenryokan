6f40cc41853941b8cae4e3177113ae848e890a48
Kawakami <fukabekonami@gmail.com>
2021-07-21 14:25:26 +0900

    プランのスライドショー部分作成

---
+++ assets/js/main.js
@@ -0,0 +1,13 @@
+
+/******************************************************************************
+ initializing slick 
+******************************************************************************/
+$(document).ready(function(){
+    $('.plan-contents').slick({
+        autoplay: true,
+        infinite: true,
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        speed: 5000,
+    });
+  });
